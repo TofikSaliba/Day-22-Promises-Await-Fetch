fed66a4c641d122b349b1b556adee8fe3390305e
TofikSaliba <tofik.saliba@gmail.com>
2022-05-09 17:32:32 +0300

    added ex 29.1 and 29.2, 28.1 correction

---
+++ Ex-29.1.js
@@ -0,0 +1,30 @@
+const getIDs = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve([532, 543, 753, 1, 5]);
+    }, 1500);
+  });
+
+const getRecipe = (recipeID) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(
+      (ID) => {
+        const recipe = {
+          title: "Fresh tomato pasta",
+          publisher: "Pinchas Hodadad",
+        };
+        resolve(`${ID}: ${recipe.title}`);
+      },
+      1500,
+      recipeID
+    );
+  });
+};
+
+const refactor = async () => {
+  const ids = await getIDs();
+  console.log(ids);
+  const recipe = await getRecipe(ids[2]);
+  console.log(recipe);
+};
+refactor();
